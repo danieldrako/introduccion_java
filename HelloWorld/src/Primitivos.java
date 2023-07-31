3fc759e18aa1ad0e749d2783263b84c632651f85
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2023-07-30 23:01:53 -0600

    Integer Numeric Primitives

---
+++ HelloWorld/src/Primitivos.java
@@ -0,0 +1,37 @@
+public class Primitivos {
+    public static void main(String[] args) {
+
+        byte numeroByte = 127;
+        System.out.println("numeroByte " + numeroByte);
+        System.out.println("corresponde en byte a  = " + Byte.BYTES);
+        System.out.println("corresponde en bites a = " + Byte.SIZE);
+        System.out.println("valor máximo de un byte = " + Byte.MAX_VALUE);
+        System.out.println("valor mínimo de un bye = " + Byte.MIN_VALUE);
+
+        short numeroShort = 32767;
+        System.out.println("numeroShort " + numeroShort);
+        System.out.println("corresponde en byte a  = " + Short.BYTES);
+        System.out.println("corresponde en bites a = " + Short.SIZE);
+        System.out.println("valor máximo de un byte = " + Short.MAX_VALUE);
+        System.out.println("valor mínimo de un bye = " + Short.MIN_VALUE);
+
+        int numeroInt = 2147483647;
+        System.out.println("numeroInt " + numeroInt);
+        System.out.println("corresponde en byte a  = " + Integer.BYTES);
+        System.out.println("corresponde en bites a = " + Integer.SIZE);
+        System.out.println("valor máximo de un byte = " + Integer.MAX_VALUE);
+        System.out.println("valor mínimo de un bye = " + Integer.MIN_VALUE);
+
+        long numeroLong = 9223372036854775807L;
+        System.out.println("numeroLong " + numeroLong);
+        System.out.println("corresponde en byte a  = " + Long.BYTES);
+        System.out.println("corresponde en bites a = " + Long.SIZE);
+        System.out.println("valor máximo de un byte = " + Long.MAX_VALUE);
+        System.out.println("valor mínimo de un bye = " + Long.MIN_VALUE);
+
+
+
+
+
+    }
+}
