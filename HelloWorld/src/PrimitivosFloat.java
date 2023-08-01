837db690f5b81e755650a217465d774d689d9fff
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2023-07-31 19:40:15 -0600

    after floating point numeric primitives

---
+++ HelloWorld/src/PrimitivosFloat.java
@@ -0,0 +1,7 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 1.5e-3f; //20120f;
+        System.out.println("realFloat = " + realFloat);
+    }
+}
