e45163908dc77090819dba8e8545bd87097f8fa2
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2023-08-18 08:16:51 -0600

    add SistemasNumericos

--- HelloWorld/src/PrimitivosBoolean.java
+++ HelloWorld/src/PrimitivosBoolean.java
@@ -4,7 +4,7 @@ public class PrimitivosBoolean {
         System.out.println("datoLogico = " + datoLogico);
         double d = 98765.43e-3;
         System.out.println("d = " + d);
-        
+
         float f = 1.2345e2f;
         System.out.println("f = " + f);
         
